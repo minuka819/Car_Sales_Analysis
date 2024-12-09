5a3e2565b27c4d37cc54abd0f2f3349b797486b3
minuka819 <minuka819@gmail.com>
2024-12-09 14:47:23 -0500

    Added new date column To do next: Dev for loop for if sale date is nan or if sellling price is nan -> drop them

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import  pandas as pd
+import pandas as pd
+import warnings
+
+
+warnings.filterwarnings("ignore")
 '''
 DATA CLEANING
 
@@ -12,36 +16,35 @@ To clean the car_sales data set for SQL database integration we have two goals:
     - Replace NAN with 'unknown' for string types 
 (2) Type cast data types of columns to appropriate data types
     - Headers including year, condition, odometer, mmr and selling price are type casted to int
-    - date type is converted to date
+    - Date column needs to be split into date and time column
     - All other headers are converted to string types 
     
 CSV file is output and ready for SQL integration
 '''
-
-
-
 #setting default values for data frame visualization
 
 df = pd.read_csv("D:\\Data_science\\Projects\\My_Car_Sales\\car_prices.csv")
 
 #inspect the first 80 rows, through visual insepction row#74 had an empty cell case
-#print(df.info())
 
 #print(df.head(80))
+
 #I imported csv but most of the strings are objects , will this be a problem in the future
-
-#print(df.to_string(pd))
-
-#change object to string
-
-#df['make'] = df['make'].astype("object")
 
 #making a copy of the data frame so that original data frame is a reference point for cleaning stages
 df_copy = df.copy(deep=True)
 
-print(df_copy.info())
+#using regex to extract the day date and time values
+df_copy[['Day', 'Date', 'Time']] = df_copy['saledate'].str.extract(
+    r'(\w+)\s+(\w+\s+\d+\s+\d+)\s+([\d:]+)'
+)
 
-#for loop for converting column types to what is required
+#Noticed that subsection of jettas with trim SE PZEV w/connectivity are incomplete , thus decided to remove these rows
+jettas = df_copy[df_copy['trim'] == 'SE PZEV w/Connectivity']
+#print(jettas)
+df_copy.drop(df_copy[df_copy.trim == 'SE PZEV w/Connectivity'].index)
+
+#For loop for converting column types to what is required
 for column in df_copy:
     if (column == 'year') or (column == 'condition') or (column == 'odometer') or (column == 'mmr') or (column == 'sellingprice'):
         df_copy[column].fillna(0,inplace=True)
@@ -50,28 +53,41 @@ for column in df_copy:
         df_copy[column].fillna("unknown",inplace=True)
         df_copy[column] = df_copy[column].astype('string')
 
-jettas = df_copy[df_copy['trim'] == 'SE PZEV w/Connectivity']
+df_copy['Date'].fillna(0,inplace=True)
+df_copy['Date'] = df_copy['Date'].str.replace(' ','-', regex=True)
+#for value in df_copy['Date']:
 
-#print(jettas)
+#    print(value)
 
+
+#change Date string to dateype
+print(df_copy['Date'])
+df_copy['Date'] = pd.to_datetime(df_copy['Date'], format='%b-%d-%Y')
+#print(df_copy['Date'])
 #print(df_copy.info())
 
-df_copy.drop(df_copy[df_copy.trim == 'SE PZEV w/Connectivity'].index)
+'''
 
-#we found 26 Volkswaggen jettas with no VIN , incorrect sales date and no mmmr value so we decided to remove these from the dataset
-#print(df_copy.info())
+#removal of sale date column
+df = df_copy.drop('saledate', axis=1)
 
 print(df_copy.info())
 
+print(df_copy)
+
 #checking for null values
 
-print('Any null values?\n',df.isnull().values.any(),'\n Null report:\n', df[df.columns[df.isnull().any()]].isnull().sum())
+print('Any null values for original?\n',df.isnull().values.any(),'\n Null report for original:\n', df[df.columns[df.isnull().any()]].isnull().sum(),'\n')
 
-print('\nNull Report for new:\n',df_copy.isnull().values.any())
-'''
+print('Any null values for new?\n',df_copy.isnull().values.any(),'\n Null report for original:\n', df_copy[df.columns[df.isnull().any()]].isnull().sum())
+
+
+
 In this project we want to test whether the condition of the car,the year and the odomoter has an effect on the
 selling price and mmr
 
 Correlation testing in Pandas 
+
 '''
+
 
